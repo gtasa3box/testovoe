7cc1617038c0e293b61bdb11f150ed6e8ee0b578
gtasa3box <gtasa3box@gmail.com>
2021-03-03 15:30:18 +0300

    1commit3

--- app/index.js
+++ app/index.js
@@ -1,3 +1,36 @@
 import './index.scss';
 
+$('.carusel').slick({
+	dots: false,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	responsive: [
+		{
+		breakpoint: 1024,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: true
+		}
+		},
+		{
+		breakpoint: 600,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+		},
+		{
+		breakpoint: 480,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		}
+		}
+	]
+	});
+
+
 console.log('Boilerplate is working!');
